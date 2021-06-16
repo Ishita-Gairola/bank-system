5ce1b81148ea2eaf0ae5a63b51bde79136d27bee
Ishita-Gairola <ishitagairola777@gmail.com>
2021-06-16 19:02:44 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Sparks Foundation Task</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  <div class='container-fluid px-0'>
+
+      <!-- Introduction section -->
+            <div class="row ">
+            <div class="col-align-self-start ">
+                <img src="img/Corporate Professional LinkedIn Banner.png" class="img-fluid " >
+              </div>
+              <div class="col">
+              <div class="heading text-center my-5">
+                  
+                  <h1>TSF BANK</h1><br><br>
+                  <h2>Transactions at your fingertips.</h2>
+                </div>
+
+              </div>
+</div>   
+             
+            
+
+      <!-- Activity section -->
+      <div class="bg-image p-5 rounded mb-5 img-fluid w-100 " style="background-image: url('img/white-background-with-triangle-patterns_1017-18410.jpg');
+                        
+                         height:55vh;">
+      <div class="row">
+              <h2 style="padding-left:10%; padding-bottom:1%; text-align:centre;"><strong>Our Services</strong></h2>
+              </div>
+            <div class="row activity text-center">
+                  <div class="col-md act">
+                    <img src="img/userImage_2_200x200.png" class="img-fluid">
+                    <br>
+                    <a href="createuser.php"><br><button>Create a User</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/money-transfer.jpg" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><br><button>Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/purchase.png" class="img-fluid">
+                    <br>
+                    <a href="transactionhistory.php"><br><button>Transfer History</button></a>
+                  </div>
+            </div>
+      </div>
+</div>
+  
+
+      <footer class="text-center mt-5 py-2">
+        <p>&copy 2021. By <b>Ishita Gairola</b></p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
